33bf1ca864be47ecf3109291fb0f90a93aaec7f5
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-04-08 09:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+
+
+
+	for(let index in sampleObject){
+		 if(key == index){
+			return true;
+		 }	
+	}
+	return false;
+	
 }
 
 // Do not change the code below
